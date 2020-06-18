bd1af4fc2aa6438575c5b6be4ba4974193c43a90
caohai <caohai@kankan.com>
2020-06-18 17:56:16 +0800

    更新vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -17,26 +17,42 @@ const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
+  /** 
+   * 部署应用时的基本URL，一般使用 '/'
+   * 如果你部署在站点的子路径下，需要设置publicPath属性
+   * 比如你打算部署项目到站点 https://foo.github.io/test/ ，子路径就是/bar/，需要 publicPath设置为'/test/'
+   * 更多参考 https://cli.vuejs.org/config/#publicpath
+  */
   // publicPath: '/',
   publicPath: '/test/',
+
+  // 构建文件的存放目录
   outputDir: 'dist',
-  assetsDir: 'static',
+
+  // 编译后，静态资源存放目录
+  assetsDir: 'static', 
+
+  // eslint 是否在保存时候进行检查，如果你只想在开发环境时使用，可以用如下配置
   lintOnSave: process.env.NODE_ENV === 'development',
+
+  // 是否在生产环境开启SourceMap，设为false，打包时不生成.map文件
   productionSourceMap: false,
+  // webpack-dev-server 相关配置，
   devServer: {
+    // 端口地址
     port: port,
+    // 是否自动自动打开浏览器
     open: true,
+
+    // 浏览器同时显示警告和错误
     overlay: {
       warnings: false,
       errors: true
     },
+    // 代理设置，可以用于开发阶段处理跨域请求
+    // proxy: {}
+    
+    // 使用mock数据
     before: require('./mock/mock-server.js')
   },
   configureWebpack: {
@@ -44,6 +60,7 @@ module.exports = {
     // it can be accessed in index.html to inject the correct title.
     name: name,
     resolve: {
+      // 设置一些常用的别名
       alias: {
         '@': resolve('src')
       }
@@ -52,12 +69,13 @@ module.exports = {
   chainWebpack(config) {
     // 内联要关掉 preload 和 prefetch，否则index.html会引入请求runtimeChunk的preload link
     // 参考issues https://github.com/PanJiaChen/vue-element-admin/issues/2690
-    // it can improve the speed of the first screen, it is recommended to turn on preload
+    // 移除预加载
     config.plugins.delete('preload')
-    // when there are many pages, it will cause too many meaningless requests
+  
+    // 移除 prefetch 插件，减少首屏加载
     config.plugins.delete('prefetch')
 
-    // set svg-sprite-loader
+    // 设置svg精灵图loader
     config.module
       .rule('svg')
       .exclude.add(resolve('src/icons'))
@@ -74,7 +92,9 @@ module.exports = {
       })
       .end()
 
-    // set preserveWhitespace
+    // true这意味着编译好的渲染函数会保留所有 HTML 标签之间的空格
+    // 如果设置为 false，则标签之间的空格会被忽略。
+    // 这能够略微提升一点性能但是可能会影响到内联元素的布局。
     config.module
       .rule('vue')
       .use('vue-loader')
@@ -92,7 +112,8 @@ module.exports = {
             .plugin('ScriptExtHtmlWebpackPlugin')
             .after('html')
             .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
+              // inline 的name 和你 runtimeChunk 的 name保持一致
+              // 将runtime内联到index.html中
               inline: /runtime\..*\.js$/
             }])
             .end()
@@ -104,22 +125,23 @@ module.exports = {
                   name: 'chunk-libs',
                   test: /[\\/]node_modules[\\/]/,
                   priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
+                  chunks: 'initial' // 只打包初始时所依赖的第三方
                 },
                 elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                  name: 'chunk-elementUI', // 将elementui拆分到一个单独的包中
+                  priority: 20, // 权重要大于 libs 和 app, 不然会被打包进 libs 或者 app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // 为了适配cnpm
                 },
                 commons: {
                   name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
-                  minChunks: 3, //  minimum common number
+                  test: resolve('src/components'), // 可自定义拓展你的规则
+                  minChunks: 3, // 最小共用次数
                   priority: 5,
                   reuseExistingChunk: true
                 }
               }
             })
+          // 将包含chunks 映射关系的 list单独从 app.js里提取出来
           config.optimization.runtimeChunk('single')
         }
       )
