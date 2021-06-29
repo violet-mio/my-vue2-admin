53024018d90d819f27ffab2ac8522b664e321ea1
caohai <1106225027@qq.com>
2021-06-29 12:02:29 +0800

    抽取前台页面组件

---
+++ src/PagesLayout/Footer.vue
@@ -0,0 +1,78 @@
+<template>
+  <footer>
+    <!-- 底部 -->
+    <slot name="footer">
+      <div class="legal">
+        <ul class="links">
+          <li class="copyright">
+            <span class="copyright-text">© 2021 - 2050 obbo 版权所有 粤ICP备000xxxx号</span>
+          </li>
+          <li><a class="link" href="#" data-analytics-title="privacy policy">隐私政策</a></li>
+          <li><a class="link" href="#" data-analytics-title="terms of use">使用条款</a></li>
+          <li><a class="link" href="#" data-analytics-title="legal">法律信息</a></li>
+          <li><a class="link" href="/sitemap" data-analytics-title="site map">网站地图</a></li>
+        </ul>
+      </div>
+    </slot>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'PageFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/styles/variables.scss';
+
+$containerWidth: 1200px;
+footer {
+  width: $containerWidth;
+  margin: 20px auto 0;
+
+  .legal {
+    position: relative;
+    padding: 26px 0;
+
+    &:before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0px;
+      right: 0px;
+      height: 1px;
+      background-color: rgba(0,0,0,0.08);
+    }
+
+    ul,
+    li {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+    }
+
+    .links {
+      display: flex;
+      align-items: center;
+
+      li {
+        display: inline-block;
+        vertical-align: baseline;
+        margin-right: 20px;
+
+        .link,
+        .copyright-text {
+          font-size: 14px;
+          font-weight: 500;
+          color: #7f7f7f;
+          line-height: 24px;
+        }
+        .link:hover {
+          color: #000
+        }
+      }
+    }
+  }
+}
+</style>
--- src/PagesLayout/index.vue
+++ src/PagesLayout/index.vue
@@ -1,98 +1,32 @@
 <template>
   <div class="page-wrapper">
     <div class="page-container">
-      <header class="header">
-        <div class="container">
-          <h1 class="logo">
-            <router-link to="/">
-              <img :src="logoImg" alt="网站首页">
-            </router-link>
-          </h1>
-          <ul class="nav">
-            <li v-for="item in menuList" :key="item.id" class="nav-item" :class="{ 'active': isActive(item) }">
-              <router-link class="link" :to="item.path">{{ item.label }}</router-link>
-            </li>
-            <li class="nav-item action-conter">
-              <el-dropdown v-if="token" trigger="click">
-                <div class="el-dropdown-link">
-                  <img src="https://sf1-ttcdn-tos.pstatp.com/img/user-avatar/3d4456d29fa60c097698e56c1c180126~300x300.image" alt="头像" class="avator">
-                </div>
-                <el-dropdown-menu slot="dropdown">
-                  <router-link to="/dashboard">
-                    <el-dropdown-item icon="el-icon-s-home">后台中心</el-dropdown-item>
-                  </router-link>
-                  <el-dropdown-item divided icon="el-icon-switch-button" @click.native="logout">退出</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-              <router-link v-else to="/login">
-                <el-button type="primary" size="medium" plain>登录</el-button>
-              </router-link>
-            </li>
-          </ul>
-        </div>
-      </header>
+      <PageHeader />
+      <!-- 首页 -->
       <main>
-        <router-view />
+        <!-- 主要内容区域 -->
+        <slot name="main" />
       </main>
-      <footer>
-        Footer
-      </footer>
+      <PageFooter />
     </div>
   </div>
 </template>
 <script>
-import logoImg from './page-logo.png'
-import { mapGetters } from 'vuex'
+import PageHeader from './Header'
+import PageFooter from './Footer'
 
 export default {
   name: 'PagesLayout',
-  data() {
-    return {
-      menuList: [
-        {
-          id: 1,
-          label: '首页',
-          path: '/home',
-          alias: ['/']
-        },
-        {
-          id: 2,
-          label: '处理中心',
-          path: '/home-info'
-        }
-      ],
-      logoImg
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'token'
-    ]),
-    activeMenu() {
-      const route = this.$route
-      const { meta, path } = route
-      // 如果meta设置激活菜单，就会高亮对应的菜单
-      if (meta.activeMenu) {
-        return meta.activeMenu
-      }
-      return path
-    }
-  },
-  methods: {
-    isActive(item) {
-      return this.activeMenu === item.path ||
-      (Array.isArray(item.alias) && item.alias.includes(this.activeMenu))
-    },
-    async logout() {
-      await this.$store.dispatch('user/logout')
-      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
-    }
+  components: {
+    PageHeader,
+    PageFooter
   }
 }
 </script>
 <style lang="scss" scoped>
 @import '~@/styles/variables.scss';
 
+$containerWidth: 1200px;
 .page-wrapper {
   position: relative;
   height: 100%;
@@ -102,94 +36,6 @@ export default {
   .page-container {
     padding: 0;
     margin: 0 auto;
-    $containerWidth: 1200px;
-
-    .header {
-      background-color: #fff;
-      .container {
-        max-width: $containerWidth;
-        margin: auto;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 16px;
-
-        .logo {
-          width: 80px;
-          height: 80px;
-          margin: 0;
-          font-size: 32px;
-          font-weight: 400;
-
-          a,
-          img {
-            width: 100%;
-            height: 100%;
-            transform: scale(0.8);
-          }
-        }
-
-        .nav {
-          display: flex;
-          align-items: center;
-          height: 100%;
-          line-height: 80px;
-          background: transparent;
-          padding: 0;
-          margin: 0;
-
-          .nav-item {
-            margin: 0;
-            list-style: none;
-            position: relative;
-
-            .link {
-              padding: 0 20px;
-              text-decoration: none;
-              cursor: pointer;
-              color: #909090;
-            }
-
-            &:hover,
-            &.active {
-              .link {
-                color: $menuActiveText;
-              }
-            }
-          }
-
-          .action-conter {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            .el-dropdown-link {
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              cursor: pointer;
-
-              &:hover {
-                color: $menuActiveText;
-              }
-              .avator {
-                display: inline-block;
-                vertical-align: middle;
-                height: 30px;
-                border-radius: 50%;
-              }
-            }
-            .el-icon-arrow-down {
-              font-size: 12px;
-            }
-            .el-dropdown-menu {
-              a {
-                display: inline-block;
-              }
-            }
-          }
-        }
-      }
-    }
 
     main {
       min-height: 200px;
@@ -197,12 +43,6 @@ export default {
       padding: 0 16px;
       margin: 20px auto 0;
     }
-
-    footer {
-      width: $containerWidth;
-      margin: 20px auto 0;
-    }
   }
-
 }
 </style>
