027f8ff36602e269574e6b0a1b177876a088f0ae
violet-mio <caohai648@qq.com>
2021-11-03 17:15:08 +0800

    更新：用户mock 头像

--- mock/user.js
+++ mock/user.js
@@ -12,13 +12,13 @@ const users = {
   'admin-token': {
     roles: ['admin'],
     introduction: 'I am a super administrator',
-    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    avatar: 'https://avatars.githubusercontent.com/u/22009607?v=4',
     name: 'Super Admin'
   },
   'editor-token': {
     roles: ['editor'],
     introduction: 'I am an editor',
-    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    avatar: 'https://avatars.githubusercontent.com/u/22009607?v=4',
     name: 'Normal Editor'
   }
 }
